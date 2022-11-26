3fd6edded314dae134a7ec7ff15576c71ac313e1
Evelyne <e.umubyeyi@alustudent.com>
2022-11-27 00:06:05 +0200

    Added delete and update functionalities for project

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import Clients from'./components/Clients'
-import AddClientsModel from './components/AddClientsModel';
+import Home from './pages/Home';
+import Project from './pages/Project'; 
+import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -30,15 +32,20 @@ const client = new ApolloClient({
 function App() {
   return (
     <>
-    <ApolloProvider client={client}>
-    <Header/>
-     <div className="container">
-        <AddClientsModel/> 
-        <Clients/>
-      </div>
-    </ApolloProvider>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
+      </ApolloProvider>
     </>
-   
+
   );
 }
 
